99215398f7c2cefbd27c5b0d66affce6ed8e492b
Sabab Asmae <sababasmae@gmail.com>
2026-03-21 19:19:17 +0000

    initializing project

---
+++ src/test/java/ma/payflow/payment_engine/PaymentEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.payflow.payment_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
